3354de5b867b0aaa3e5700d3248d79b5f9f04740
rolandkiss0925 <kissroland0925@mailbox.unide.hu>
2022-07-05 22:14:42 +0200

    Delete commit

--- first_task/src/main/java/com/example/first_task/repository/ServicePointRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.first_task.repository;
-
-import com.example.first_task.entity.ServicePoint;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ServicePointRepository extends CrudRepository<ServicePoint, Long> {
-}
