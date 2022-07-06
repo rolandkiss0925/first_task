7c5b024d58a635ce7f1e883854057cb1123b7071
rolandkiss0925 <kissroland0925@mailbox.unide.hu>
2022-07-06 11:38:48 +0200

    Swagger works

--- src/main/java/com/example/first_task/FirstTaskApplication.java
+++ src/main/java/com/example/first_task/FirstTaskApplication.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableSwagger2
 public class FirstTaskApplication {
 
 	public static void main(String[] args) {
